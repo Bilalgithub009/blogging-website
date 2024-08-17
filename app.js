15f0631b32da22941582b381adc78baa25b53bfe
Bilalgithub009 <m.bilaljaliyawala@gmail.com>
2024-08-17 16:19:15 +0500

    code
    bloging web push

---
+++ app.js
@@ -0,0 +1,6 @@
+ // Optional: Customize the interval or other carousel options using JavaScript
+//  var heroCarousel = document.querySelector('#heroCarousel');
+//  var carousel = new bootstrap.Carousel(heroCarousel, {
+//      interval: 5000,  // Time between slides in milliseconds (5000ms = 5s)
+//      pause: 'hover'  // Pause the carousel on hover
+//  });
